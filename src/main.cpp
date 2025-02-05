fd8076b8e97ff2618cf5e623c98a7c90c9ade6f5
Nils Engelbach <me@nilsengelbach.com>
2025-02-05 18:06:48 +0100

    Use BSEC Software Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
+#include <umm_malloc/umm_heap_select.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME680.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <Servo.h>
-#include <FS.h>
-#include <LittleFS.h>
+#include <bsec.h>
 
 #include "secrets.h"
 
-Adafruit_BME680 bme; // I2C
+// Functions
+void checkSensorStatus(void);
+
+// Global variables
+Bsec bme;
+String output;
 
 void setup() {
     Serial.begin(115200);
@@ -18,42 +18,80 @@ void setup() {
 
     Serial.println("Starting...");
 
-    if (!bme.begin()) {
-        Serial.println("BME688 nicht gefunden!");
-        while (1);
-    }
-    
-    bme.setTemperatureOversampling(BME680_OS_8X);
-    bme.setHumidityOversampling(BME680_OS_2X);
-    bme.setPressureOversampling(BME680_OS_4X);
-    bme.setGasHeater(320, 150);
+    bme.begin(BME68X_I2C_ADDR_HIGH, Wire);
+    output = "\nBSEC library version " + String(bme.version.major) + "." + String(bme.version.minor) + "." + String(bme.version.major_bugfix) + "." + String(bme.version.minor_bugfix);
+    Serial.println(output);
+    checkSensorStatus();
+
+    bsec_virtual_sensor_t sensorList[13] = {
+        BSEC_OUTPUT_IAQ,
+        BSEC_OUTPUT_STATIC_IAQ,
+        BSEC_OUTPUT_CO2_EQUIVALENT,
+        BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+        BSEC_OUTPUT_RAW_TEMPERATURE,
+        BSEC_OUTPUT_RAW_PRESSURE,
+        BSEC_OUTPUT_RAW_HUMIDITY,
+        BSEC_OUTPUT_RAW_GAS,
+        BSEC_OUTPUT_STABILIZATION_STATUS,
+        BSEC_OUTPUT_RUN_IN_STATUS,
+        BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+        BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+        BSEC_OUTPUT_GAS_PERCENTAGE
+    };
+
+    bme.updateSubscription(sensorList, 13, BSEC_SAMPLE_RATE_LP);
+    checkSensorStatus();
+
+    // Print the header
+    output = "Timestamp, IAQ, IAQ accuracy, Static IAQ, CO2 equivalent, breath VOC equivalent, raw temp[°C], pressure [Pa], raw relative humidity [%], gas [Ohm], Stab Status, run in status, comp temp[°C], comp humidity [%], gas percentage";
+    Serial.println(output);
 }
 
-void loop() {
-    // Perform a measurement
-    if (!bme.performReading()) {
-        Serial.println("Failed to perform reading");
-        return;
+void loop(void)
+{
+  unsigned long time_trigger = millis();
+  if (bme.run()) {
+    output = String(time_trigger);
+    output += ", " + String(bme.iaq);
+    output += ", " + String(bme.iaqAccuracy);
+    output += ", " + String(bme.staticIaq);
+    output += ", " + String(bme.co2Equivalent);
+    output += ", " + String(bme.breathVocEquivalent);
+    output += ", " + String(bme.rawTemperature);
+    output += ", " + String(bme.pressure);
+    output += ", " + String(bme.rawHumidity);
+    output += ", " + String(bme.gasResistance);
+    output += ", " + String(bme.stabStatus);
+    output += ", " + String(bme.runInStatus);
+    output += ", " + String(bme.temperature);
+    output += ", " + String(bme.humidity);
+    output += ", " + String(bme.gasPercentage);
+    Serial.println(output);
+  } else {
+    checkSensorStatus();
+  }
+}
+
+
+void checkSensorStatus(void)
+{
+  if (bme.bsecStatus != BSEC_OK) {
+    if (bme.bsecStatus < BSEC_OK) {
+      output = "BSEC error code : " + String(bme.bsecStatus);
+      Serial.println(output);
+    } else {
+      output = "BSEC warning code : " + String(bme.bsecStatus);
+      Serial.println(output);
     }
+  }
 
-    // Print sensor values
-    Serial.print("Temperature = ");
-    Serial.print(bme.temperature);
-    Serial.println(" *C");
-
-    Serial.print("Pressure = ");
-    Serial.print(bme.pressure / 100.0);
-    Serial.println(" hPa");
-
-    Serial.print("Humidity = ");
-    Serial.print(bme.humidity);
-    Serial.println(" %");
-
-    Serial.print("Gas Resistance = ");
-    Serial.print(bme.gas_resistance);
-    Serial.println(" ohms");
-
-    Serial.println();
-
-    delay(2000); // Wait 2 seconds before next read
+  if (bme.bme68xStatus != BME68X_OK) {
+    if (bme.bme68xStatus < BME68X_OK) {
+      output = "BME68X error code : " + String(bme.bme68xStatus);
+      Serial.println(output);
+    } else {
+      output = "BME68X warning code : " + String(bme.bme68xStatus);
+      Serial.println(output);
+    }
+  }
 }
